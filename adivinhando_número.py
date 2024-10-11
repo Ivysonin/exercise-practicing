16816da4582ae662e97910f67c26827a7e1e6bde
Ivyson Thauan <ivysonthauan17@gmail.com>
2024-10-11 15:47:56 -0300

    exercicio

--- adivinhando_número.py
+++ adivinhando_número.py
@@ -2,18 +2,20 @@
 
 import random #aleátorio
 numero_secreto = random.randint (1,100)
+tentativas = 1
 
-print ("===== Tentando acertar um número secreto entre (1,100) =====")
-numero = int(input ("Digite um palpite: "))
+print ("\n===== Tentando acertar um número secreto entre (1,100) =====\n")
+palpite = int(input ("Digite um palpite: "))
 
 # Sistema de loop até acerta o número correto 
 
-while numero != numero_secreto:
-    print ("===== Você não acertou o número, tente novamente =====")
-    if numero < numero_secreto:
+while palpite != numero_secreto:
+    print ("===== Você não acertou o número, tente novamente =====\n")
+    if palpite < numero_secreto:
         print ("Dica: O número é maior !")
     else:
         print ("Dica: O número é menor !")
-    numero = int(input ("Digite um palpite: "))
+    palpite = int(input ("Digite um palpite: "))
+    tentativas += 1
 
-print (f"========== Parabéns! Você acertou o número secreto é {numero_secreto} ==========")
+print (f"\n========== Parabéns! Você acertou em {tentativas} tentativas, O número secreto é {numero_secreto} ==========")
